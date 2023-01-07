356a369fe9901000be1ccc7808ef2e015e0a8e79
Liu <binbin.liu@dell.com>
2023-01-07 18:42:12 -0500

    fix a typo in auth config

--- packages/auth/config/webpack.prod.js
+++ packages/auth/config/webpack.prod.js
@@ -11,7 +11,7 @@ const prodConfig = {
     },
     plugins: [
         new ModuleFederationPlugin({
-            name: 'marketing',
+            name: 'auth',
             filename: 'remoteEntry.js',
             exposes: {
                 './Auth': './src/bootstrap'

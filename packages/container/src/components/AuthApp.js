c7afcd0e8e90e4ef4c509fc38aca00e4eaa995d2
Liu <binbin.liu@dell.com>
2023-01-07 20:01:06 -0500

    add centralize authorization management

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -2,7 +2,7 @@ import {mount} from 'auth/Auth';
 import React, {useRef, useEffect} from 'react';
 import {useHistory} from 'react-router-dom';
 
-export default () => {
+export default ({onSignIn}) => {
 
     const ref = useRef(null);
     const history = useHistory();
@@ -17,6 +17,11 @@ export default () => {
               history.push(nextPathname);
             }
           },
+          onSignIn: () => {
+
+           onSignIn();
+
+          }
         });
     
         history.listen(onParentNavigate);
